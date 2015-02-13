1e577c1fe45c886f04e4783c9d0eabe85e0c8d21
Adam Koczka <Adam_Koczka@epam.com>
2015-02-13 15:29:01 +0100

    unneeded notifies removed

--- src/mt/ringbuffer/GoodRingBuffer.java
+++ src/mt/ringbuffer/GoodRingBuffer.java
@@ -6,37 +6,52 @@ public class GoodRingBuffer implements RingBuffer {
     private long readCount;
     private long writeCount;
     
+    private final Object guard = new Object();
+    
+    private boolean readersWaiting = false;
+    private boolean writersWaiting = false;
+    
     public GoodRingBuffer(int size) {
         buffer = new int[size];
     }
     
 	@Override
-	public synchronized int  get() throws InterruptedException {		
+	public int  get() throws InterruptedException {		
+		synchronized(guard){
 			while(readCount >= writeCount) {
-				wait();
+				readersWaiting = true;
+				guard.wait();
 			}
 				
 			int readIndex = (int) (readCount % buffer.length);
 	        readCount++;
-	        if(writeCount < readCount + buffer.length){
-				notifyAll();
+	        if(writeCount < readCount + buffer.length && writersWaiting){
+	        	readersWaiting = false;
+				writersWaiting = false;
+				guard.notifyAll();
 			}
 	        return buffer[readIndex];
+		}
 	}
 
 	@Override
-	public synchronized void put(int value) throws InterruptedException {	
+	public void put(int value) throws InterruptedException {
+		synchronized(guard){
 			while(writeCount >= readCount + buffer.length){
-				wait();
+				writersWaiting = true;
+				guard.wait();	
 			}
 				
 			int writeIndex = (int) (writeCount % buffer.length);
-	        buffer[writeIndex] = value;
-	        writeCount++;
-	        
-	        if(readCount < writeCount){
-				notifyAll();
+			buffer[writeIndex] = value;
+			writeCount++;
+			
+			if(readCount < writeCount && readersWaiting){
+				readersWaiting = false;
+				writersWaiting = false;
+				guard.notify();
 			}
+		}
 	}
 
 }
